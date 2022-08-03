c27e27e92825ba5aed85308453b4fbe0756e28ae
itzdonaldxd <donaldl@mit.edu>
2022-08-02 22:45:16 -0400

    Implemented a working snake that moves

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 import random
  
 snake_speed = 15
+snake_size = 20
+fruit_size = 10
  
 # Window size
 window_x = 720
@@ -42,3 +44,67 @@ fruit_spawn = True
 # setting default snake direction towards right
 direction = 'RIGHT'
 change_to = direction
+
+# # Main Function
+
+while True:
+    
+    # handling key events
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            if event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            if event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            if event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+            # print(change_to)
+ 
+    # If two keys pressed simultaneously
+    # we don't want snake to move into two directions
+    # simultaneously
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    if change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    if change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    if change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+        
+ 
+    # Moving the snake
+    if direction == 'UP':
+        snake_position[1] -= snake_size
+    if direction == 'DOWN':
+        snake_position[1] += snake_size
+    if direction == 'LEFT':
+        snake_position[0] -= snake_size
+    if direction == 'RIGHT':
+        snake_position[0] += snake_size
+    
+    # Insert the new snake position
+    snake_body.insert(0, list(snake_position))
+    
+    # Remove the old snake position
+    snake_body.pop()
+    
+    # Erase the old snake position from the screen
+    game_window.fill(black)
+ 
+	# Drawing the snake at the newest position
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green, 
+                         pygame.Rect(pos[0], pos[1], snake_size, snake_size))
+    
+    # Refresh game screen
+    pygame.display.update()
+    
+    # Frame Per Second /Refresh Rate
+    fps.tick(snake_speed)
+
+
+
+
